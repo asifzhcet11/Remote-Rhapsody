8bfbac3d2d4ba2fcc5576fbae7c64cbaab9be8ba
mohammadasif <asifzhcet11@gmail.com>
2020-11-30 00:43:42 +0100

    add(monpoly): adding monpoly angular app

---
+++ monplany/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+import {CalendarView, CalendarEvent, CalendarEventTimesChangedEvent} from "angular-calendar";
+import { Subject } from 'rxjs';
+import {startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours,} from 'date-fns';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'monplany';
+  view: CalendarView = CalendarView.Week;
+  calendarView = CalendarView;
+  viewDate: Date = new Date();
+  events: CalendarEvent[] = [];
+  activeDayIsOpen: boolean = true;
+  refresh: Subject<any> = new Subject();
+
+  changeView(view: CalendarView){
+    this.view = view;
+  }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      this.activeDayIsOpen = !((isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0);
+      this.viewDate = date;
+    }
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    console.log("handled event");
+  }
+
+  eventTimesChanged({
+                      event,
+                      newStart,
+                      newEnd,
+                    }: CalendarEventTimesChangedEvent): void {
+    this.events = this.events.map((iEvent) => {
+      if (iEvent === event) {
+        return {
+          ...event,
+          start: newStart,
+          end: newEnd,
+        };
+      }
+      return iEvent;
+    });
+    this.handleEvent('Dropped or resized', event);
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+}
