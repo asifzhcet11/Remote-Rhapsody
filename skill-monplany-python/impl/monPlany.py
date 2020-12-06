d9596e051e36cb2ff450f577eb58612b59943ba8
ga27voz <ga27voz@mytum.de>
2020-12-06 03:19:25 +0100

    add(monplany): add question for the first prototype

---
+++ skill-monplany-python/impl/monPlany.py
@@ -0,0 +1,66 @@
+from skill_sdk import skill, Response, tell, ask
+from skill_sdk.l10n import _
+from typing import List
+
+@skill.intent_handler('TEAM__21__HOBBIES')
+def handler() -> Response:
+    msg = _('MONPLANY_ASK_HOBBIES')
+    return tell(msg)
+
+@skill.intent_handler('TEAM__21__TELL__HOBBIES')
+def handler(hobbies: List[str]) -> Response:
+    print(hobbies)
+    try:
+        assert len(hobbies) > 0
+        msg = _('MONPLANY_TELL_HOBBIES', hobbies=len(hobbies))
+        for id, str in enumerate(hobbies):
+            if id == 0:
+                msg = msg + ' ' + str
+            elif id == len(hobbies)-1:
+                msg = msg + ' und ' + str
+            else:
+                msg = msg + ', ' + str
+        msg+= '.'
+        response = ask(msg + _("MONPLANY_ASK_SHOPPING"))
+    except(AssertionError, ValueError, TypeError):
+        msg = _('MONPLANY_EMPTY_HOBBIES')
+        response = tell(msg + _("MONPLANY_ASK_SHOPPING"))
+    return response
+
+@skill.intent_handler('TEAM__21__SHOPPING')
+def handler(shopping: int) -> Response:
+    print(shopping)
+    if shopping < 0:
+        msg = _("MONPLANY_NEGATIVE_SHOPPING_NUMBER")
+    elif shopping == 0:
+        msg = _("MONPLANY_NULL_SHOPPING_NUMBER")
+    elif shopping > 7:
+        msg = _("MONPLANY_BIG_SHOPPING_NUMBER")
+    else:
+        msg = _('MONPLANY_SHOPPING_NUMBER', number=shopping)
+
+    response = tell(msg + _("MONPLANY_ASK_GYM"))
+    return response
+
+@skill.intent_handler('TEAM__21__GYM')
+def handler(gym: bool) -> Response:
+    print(gym)
+    if gym :
+        msg = _("MONPLANY_GO_GYM")
+        response = tell(msg + _("MONPLANY_ASK_NUMBER_GYM"))
+    else:
+        msg = _("MONPLANY_NOGO_GYM")
+        response = tell(msg + _("MONPLONY_FINALIZE"))
+    return response
+
+@skill.intent_handler('TEAM__21__NUMBER__GYM')
+def handler(number_gym: int) -> Response:
+    print(number_gym)
+    if number_gym <= 0:
+        msg = _("MONPLANY_NEGATIVE_GYM_NUMBER")
+    elif number_gym > 7:
+        msg = _("MONPLANY_BIG_GYM_NUMBER")
+    else:
+        msg = _('MONPLANY_GYM_NUMBER', number=number_gym)
+    response = tell(msg + _("MONPLONY_FINALIZE"))
+    return response
